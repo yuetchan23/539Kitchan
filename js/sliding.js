1d304131a5db893748d8308f2ba0fd88d8c2d4d9
yidih <yidi@0587341825.wireless.umich.net>
2015-12-03 16:38:56 -0500

    jquery-sliding

---
+++ js/sliding.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#openbtn").click(function(){
+        $("#whowr").slideDown('slow');
+    });
+});
+
+$(document).ready(function(){
+    $("#close").click(function(){
+        $("#whowr").slideUp("slow");
+    });
+});
